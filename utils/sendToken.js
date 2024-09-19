97c68a1958877140226335dcde2a39b976be68ef
Ritik <109175397+RitikShah09@users.noreply.github.com>
2024-09-19 23:29:43 +0530

    Update sendToken.js

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -5,7 +5,7 @@ exports.sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production"?true :false,
   };
   res
     .status(statusCode)
