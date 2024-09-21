4296ebdd9fb6e2dc49c5e556df68f1880584cbdb
Ritik <109175397+RitikShah09@users.noreply.github.com>
2024-09-21 22:46:13 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,6 +4,7 @@ const ErrorHandler = require("../utils/errorHandler");
 const { catchAsyncErrors } = require("./catchAsyncErrors");
 
 exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+    console.log(req.cookies)
     const { token } = req.cookies;
     if (!token) {
         return next(new ErrorHandler("Please Login To Access The Resources", 401));
